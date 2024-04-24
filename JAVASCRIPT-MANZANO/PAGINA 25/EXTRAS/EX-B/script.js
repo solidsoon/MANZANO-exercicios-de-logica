c63161ace52ba551994758b6bac3317675b9e12d
Kauan <162659253+solidsoon@users.noreply.github.com>
2024-04-23 23:15:50 -0300

    Delete JAVASCRIPT-MANZANO/PAGINA 25/EXTRAS directory

--- JAVASCRIPT-MANZANO/PAGINA 25/EXTRAS/EX-B/script.js
+++
@@ -1,9 +0,0 @@
-let salario = parseFloat(prompt("Digite o salário do seu funcionário: "));
-let reajuste = parseFloat(prompt("Digite o valor do reajuste no salário do seu funcionário em porcentagem: "));
-
-let resultado = salario*reajuste
-
-alert("Esse é o valor do reajuste "+resultado);
-
-resultado = salario + resultado;
-alert("Seu salário com reajuste: "+resultado);
